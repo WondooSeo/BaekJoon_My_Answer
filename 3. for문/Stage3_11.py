6a9b64bc011df8ca2f88c098ced42ebb59428261
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-02-23 18:34:28 +0900

    Question no. 10871

---
+++ 3. for문/Stage3_11.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == '__main__':
+	n,x = map(int,sys.stdin.readline().rstrip().split())
+	a = list(map(int,sys.stdin.readline().rstrip().split()))
+
+	for i in range(n):
+		if a[i]<x:
+			print(a[i], end=' ')
