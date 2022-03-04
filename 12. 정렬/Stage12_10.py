bff36e952df8ab814dfce7aedb7c6272e4a9f7e4
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-03-05 04:38:20 +0900

    Question no. 18870

---
+++ 12. 정렬/Stage12_10.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == '__main__':
+    a = int(input())
+    b_list = list(map(int,sys.stdin.readline().rstrip().split()))
+    b_list_sort = list(sorted(set(b_list)))
+    dic_b = {b_list_sort[i]:i for i in range(len(b_list_sort))}
+    for i in b_list:
+        print(dic_b[i],end=' ')
