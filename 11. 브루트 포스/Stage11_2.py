82f5e344f8ce57ed97f973f4f275d3f7fc6c10d5
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-03-08 21:27:20 +0900

    Question no. 2231

---
+++ 11. 브루트 포스/Stage11_2.py
@@ -0,0 +1,13 @@
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    M = 0
+
+    for i in range(1,n):
+        i_list = list(map(int,str(i)))
+        if i + sum(i_list) == n:
+            M = i
+            break
+
+    print(M)
