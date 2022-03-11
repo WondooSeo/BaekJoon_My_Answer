8d2a9bab151fd093802fbb1763a65367c081a03f
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-03-12 01:56:54 +0900

    Question no. 10814

---
+++ 12. 정렬/Stage12_9.py
@@ -0,0 +1,13 @@
+import sys
+from collections import Counter
+
+if __name__ == '__main__':
+	N = int(sys.stdin.readline().rstrip())
+	signin_list = []
+
+	for _ in range(N):
+		signin_list.append(list(sys.stdin.readline().rstrip().split()))
+
+	signin_list = sorted(signin_list,key=lambda x: (int(x[0])))
+	for signin in signin_list:
+		print(signin[0],signin[1])
