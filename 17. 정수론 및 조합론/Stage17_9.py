7c99c3c1cec7aeea13398194f8fc2b077f879421
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-03-19 03:04:43 +0900

    Question no. 1010

---
+++ 17. 정수론 및 조합론/Stage17_9.py
@@ -0,0 +1,15 @@
+import sys
+
+def factorial(a):
+	if a>0:
+		return a*factorial(a-1)
+	else:
+		return 1;
+
+if __name__ == '__main__':
+	tc = int(sys.stdin.readline().rstrip())
+
+	for i in range(tc):
+		n,m = map(int,sys.stdin.readline().rstrip().split())
+		comb = int(factorial(max(n,m))/(factorial(min(n,m))*factorial(abs(n-m))))
+		print(comb)
