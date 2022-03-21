f792c0ce71a6fced1fcdf80b268afd4c505ef673
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-03-21 14:44:36 +0900

    Question no. 11399

---
+++ 16. 그리디 알고리즘/Stage16_3.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline().rstrip())
+    time_ATM = list(map(int,sys.stdin.readline().rstrip().split()))
+    time_ATM.sort(reverse=True)
+    time_count = 0
+
+    for i in range(N):
+        time_count += time_ATM[i]*(i+1)
+
+    print(time_count)
