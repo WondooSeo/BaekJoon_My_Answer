a5c6b5321afb539a9ee90e5171b8b770851276ab
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-03-31 02:53:19 +0900

    Question no. 1904

---
+++ 14. 동적 계획법 1/Stage14_3.py
@@ -0,0 +1,15 @@
+import sys
+
+def bin_tile(n):
+    total_tile = [1,2]
+    if n > len(total_tile):
+        for i in range(len(total_tile),n+1):
+            # mod 15476 here to avoid memory limit exceed
+            total_tile.append((total_tile[i-1]+total_tile[i-2])%15746)
+
+    return total_tile[n-1]
+
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline().rstrip())
+    print(bin_tile(N))
