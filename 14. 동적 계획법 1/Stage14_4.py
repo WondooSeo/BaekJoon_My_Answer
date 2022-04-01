b2eda21ded2ba782e13143df25051b0c30d2674e
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-04-01 22:32:04 +0900

    Question no. 9461

---
+++ 14. 동적 계획법 1/Stage14_4.py
@@ -0,0 +1,14 @@
+import sys
+
+def padoban(num):
+    padoban_list = [1,1,1]
+    if num > len(padoban_list):
+        for i in range(len(padoban_list),num):
+            padoban_list.append(padoban_list[i-2]+padoban_list[i-3])
+
+    print(padoban_list[num-1])
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline().rstrip())
+    for _ in range(N):
+        padoban(int(sys.stdin.readline().rstrip()))
