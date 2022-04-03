dd61d823b2b3a2c58d0af3752262b356e275ee4f
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:37:39 +0900

    Question no. 1065

--- 5. 함수/Stage5_3.py
+++ 5. 함수/Stage5_3.py
@@ -1,20 +1,22 @@
 import sys
 
+
 def is_hansu(a):
 	a_temp = a
 	a_list = []
 	while a_temp != 0:
 		a_list.append(a_temp%10)
 		a_temp = a_temp//10
-	if a<100:
+	if a < 100:
 		return True
-	elif a>=100 and a<=999:
+	elif a >= 100 and a <= 999:
 		if a_list[0] - a_list[1] == a_list[1] - a_list[2]:
 			return True
 		else:
 			return False
 	else:
 		return False
+
 
 if __name__ == '__main__':
 	a = int(input())
