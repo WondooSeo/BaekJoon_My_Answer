58a6a990ab45792457987513a4189a3afeb84603
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:16:50 +0900

    Question no. 3036

--- 16. 정수론 및 조합론/Stage16_6.py
+++ 16. 정수론 및 조합론/Stage16_6.py
@@ -1,9 +1,10 @@
 import sys
 
+
 def gcd(a,b):
     while b > 0:
         a = a%b
-        a,b = b,a
+        a, b = b, a
 
     return a
 
@@ -12,8 +13,8 @@ if __name__ == '__main__':
     T = int(sys.stdin.readline().rstrip())
     wheel = list(map(int, sys.stdin.readline().rstrip().split()))
 
-    for i in range(1,T):
-        now_gcd = gcd(wheel[0],wheel[i])
+    for i in range(1, T):
+        now_gcd = gcd(wheel[0], wheel[i])
         first_wheel = wheel[0] // now_gcd
         now_wheel = wheel[i] // now_gcd
         print(str(first_wheel)+'/'+str(now_wheel))
