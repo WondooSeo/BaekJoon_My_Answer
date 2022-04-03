b5572efd23d4873f8fd781d0940b380944871e77
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:17:00 +0900

    Question no. 11051

--- 16. 정수론 및 조합론/Stage16_8.py
+++ 16. 정수론 및 조합론/Stage16_8.py
@@ -1,15 +1,13 @@
 import sys
 
 if __name__ == '__main__':
-
-    N, K = map(int,sys.stdin.readline().rstrip().split())
-
+    N, K = map(int, sys.stdin.readline().rstrip().split())
     bunja = 1; bunmo = 1;
 
-    for i in range(1,K+1):
+    for i in range(1, K+1):
         bunmo *= i
 
-    for j in range(N,N-K,-1):
+    for j in range(N, N-K, -1):
         bunja *= j
 
     print((bunja//bunmo)%10007)
