ea58ddbb67b430494f5575c6c13b4e32d2c350fd
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:08:18 +0900

    Question no. 1427

--- 11. 정렬/Stage11_5.py
+++ 11. 정렬/Stage11_5.py
@@ -1,8 +1,8 @@
 import sys
 
 if __name__ == '__main__':
-	a = list(map(int,sys.stdin.readline().rstrip()))
-	a = sorted(a,reverse=True)
+	a = list(map(int, sys.stdin.readline().rstrip()))
+	a = sorted(a, reverse=True)
 
 	for num in a:
-		print(num,end='')
+		print(num, end='')
