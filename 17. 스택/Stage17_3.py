c628ca2d4fba8171299f74368e0a8f5ee4569d6e
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:20:35 +0900

    Question no. 9012

--- 17. 스택/Stage17_3.py
+++ 17. 스택/Stage17_3.py
@@ -1,5 +1,3 @@
-import sys
-
 if __name__ == '__main__':
     N = int(input())
 
@@ -7,7 +5,7 @@ if __name__ == '__main__':
         PS = list(str(sys.stdin.readline().rstrip()))
         stack = []
         for ps_i in PS:
-            if len(stack)>0 and stack[-1] == '(' and ps_i == ')':
+            if len(stack) > 0 and stack[-1] == '(' and ps_i == ')':
                 stack.pop()
             else:
                 stack.append(ps_i)
