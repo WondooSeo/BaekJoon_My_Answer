0b02fd05779f86bdd4e8c4d75f29608e2384d8dc
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:58:14 +0900

    Question no. 1978

--- 8. 기본 수학 2/Stage8_1.py
+++ 8. 기본 수학 2/Stage8_1.py
@@ -1,12 +1,13 @@
 import sys
 
+
 def prime_sleve(n):
     n_list = [1]*n
     sleve = []
 
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         if i == 1:
-            n_list[i-1] = 0;
+            n_list[i-1] = 0
 
         elif n_list[i-1] == 1:
             sleve.append(i)
@@ -20,10 +21,11 @@ def prime_sleve(n):
 
     return sleve
 
+
 if __name__ == '__main__':
     my_sleve = prime_sleve(1000)
     N = int(sys.stdin.readline().rstrip())
-    num_list = list(map(int,sys.stdin.readline().rstrip().split()))
+    num_list = list(map(int, sys.stdin.readline().rstrip().split()))
     count = 0
 
     for num in num_list:
