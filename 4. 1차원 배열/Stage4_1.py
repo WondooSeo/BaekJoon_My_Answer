f42a8fda59b63094b0c6dd05ad4518b57b6eaeae
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:34:06 +0900

    Question no. 10818

--- 4. 1차원 배열/Stage4_1.py
+++ 4. 1차원 배열/Stage4_1.py
@@ -2,5 +2,5 @@ import sys
 
 if __name__ == '__main__':
 	a = int(input())
-	a_list = list(map(int,sys.stdin.readline().rstrip().split()))
-	print(min(a_list),max(a_list))
+	a_list = list(map(int, sys.stdin.readline().rstrip().split()))
+	print(min(a_list), max(a_list))
