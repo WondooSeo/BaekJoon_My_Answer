6d72bb0ec4dbc7e40ae1899d9c52a32bad57db8f
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:58:31 +0900

    Question no. 4948

--- 8. 기본 수학 2/Stage8_5.py
+++ 8. 기본 수학 2/Stage8_5.py
@@ -1,10 +1,11 @@
 import sys
 
-def prime_sleve(a,b):
+
+def prime_sleve(a, b):
 	is_prime = [1]*b
 	sleve = []
 
-	for i in range(1,int(b**(0.5))+1):
+	for i in range(1, int(b**(0.5))+1):
 		if i == 1:
 			is_prime[i-1] = 0
 
@@ -17,7 +18,7 @@ def prime_sleve(a,b):
 		else:
 			continue
 
-	for j in range(a+1,b+1):
+	for j in range(a+1, b+1):
 		if is_prime[j-1] == 1:
 			sleve.append(j)
 
@@ -30,5 +31,5 @@ if __name__ == '__main__':
 		if n == 0:
 			break
 
-		now_sleve = prime_sleve(n,2*n)
+		now_sleve = prime_sleve(n, 2*n)
 		print(len(now_sleve))
