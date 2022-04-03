48f72b5ca917e1eb6be0f9f0932e45c18bc1d138
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:46:25 +0900

    Question no. 2869

--- 7. 기본 수학 1/Stage7_4.py
+++ 7. 기본 수학 1/Stage7_4.py
@@ -1,6 +1,6 @@
-Simport sys
+import sys
 import math
 
 if __name__ == '__main__':
-    A,B,V = map(int,sys.stdin.readline().rstrip().split())
+    A, B, V = map(int, sys.stdin.readline().rstrip().split())
     print(math.ceil((V-A)/(A-B))+1)
