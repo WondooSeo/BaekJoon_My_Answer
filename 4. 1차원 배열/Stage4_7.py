2f01c1f35d0cd015592ab34c591bcca227bbe0b3
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:34:35 +0900

    Question no. 4344

--- 4. 1차원 배열/Stage4_7.py
+++ 4. 1차원 배열/Stage4_7.py
@@ -3,11 +3,11 @@ import sys
 if __name__ == '__main__':
 	a = int(input())
 	for i in range(a):
-		testcase = list(map(int,sys.stdin.readline().rstrip().split()))
+		testcase = list(map(int, sys.stdin.readline().rstrip().split()))
 		testcase_avg = (sum(testcase)-testcase[0])/testcase[0]
 		up_avg = 0
 		for j in range(testcase[0]):
-			if testcase[j+1]>testcase_avg:
-				up_avg+=1
+			if testcase[j+1] > testcase_avg:
+				up_avg += 1
 		not_avg_p = up_avg/testcase[0]*100
 		print('%.3f' % not_avg_p + '%')
