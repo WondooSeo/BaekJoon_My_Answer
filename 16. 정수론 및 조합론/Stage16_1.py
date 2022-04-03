e658fb032772737a03daa571a50e839f8cd767d7
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:16:26 +0900

    Question no. 5086

--- 16. 정수론 및 조합론/Stage16_1.py
+++ 16. 정수론 및 조합론/Stage16_1.py
@@ -1,18 +1,14 @@
 import sys
 
 if __name__ == '__main__':
-    
     while True:
-        a,b = map(int,sys.stdin.readline().rstrip().split())
-        
-        if (a,b) == (0,0):
-            break
+        a, b = map(int, sys.stdin.readline().rstrip().split())
 
+        if (a, b) == (0, 0):
+            break
         elif a%b == 0:
             print('multiple')
-
         elif b%a == 0:
             print('factor')
-
         else:
             print('neither')
