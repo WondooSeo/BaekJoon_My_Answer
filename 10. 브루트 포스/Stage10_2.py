fcdbb053100cb23131866188f3c997411f49b46a
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:03:38 +0900

    Question no. 2231

--- 10. 브루트 포스/Stage10_2.py
+++ 10. 브루트 포스/Stage10_2.py
@@ -1,11 +1,9 @@
-import sys
-
 if __name__ == '__main__':
     n = int(input())
     M = 0
 
-    for i in range(1,n):
-        i_list = list(map(int,str(i)))
+    for i in range(1, n):
+        i_list = list(map(int, str(i)))
         if i + sum(i_list) == n:
             M = i
             break
