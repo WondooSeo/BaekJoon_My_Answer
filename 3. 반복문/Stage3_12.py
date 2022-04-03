05dd841fdbf57c9be88da47ac244a8fff187129a
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:29:31 +0900

    Question no. 10952

--- 3. 반복문/Stage3_12.py
+++ 3. 반복문/Stage3_12.py
@@ -2,9 +2,9 @@ import sys
 
 if __name__ == '__main__':
 	while True:
-		a,b = map(int,sys.stdin.readline().rstrip().split())
+		a, b = map(int, sys.stdin.readline().rstrip().split())
 		
-		if a==0 and b==0:
+		if a == 0 and b == 0:
 			break
 		else:
 			print(a+b)
