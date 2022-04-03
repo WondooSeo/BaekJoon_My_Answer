7e221cf0437896743c4f90ff349bd1cb13fe6e07
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:11:18 +0900

    Question no. 11399

--- 15. 그리디 알고리즘/Stage15_3.py
+++ 15. 그리디 알고리즘/Stage15_3.py
@@ -2,7 +2,7 @@ import sys
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline().rstrip())
-    time_ATM = list(map(int,sys.stdin.readline().rstrip().split()))
+    time_ATM = list(map(int, sys.stdin.readline().rstrip().split()))
     time_ATM.sort(reverse=True)
     time_count = 0
 
