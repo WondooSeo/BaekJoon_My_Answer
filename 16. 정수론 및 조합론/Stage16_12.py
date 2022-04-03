6d943d8e08fba2b1fc8d7a7e242bee7b3c0178c9
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:17:17 +0900

    Question no. 2004

--- 16. 정수론 및 조합론/Stage16_12.py
+++ 16. 정수론 및 조합론/Stage16_12.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def count_two(temp):
     num_two = 0
     while temp != 0:
@@ -20,6 +21,6 @@ def count_five(temp):
 
 if __name__ == '__main__':
 
-    N, M = map(int,sys.stdin.readline().rstrip().split())
+    N, M = map(int, sys.stdin.readline().rstrip().split())
 
-    print(min(count_two(N)-count_two(N-M)-count_two(M),count_five(N)-count_five(N-M)-count_five(M)))
+    print(min(count_two(N)-count_two(N-M)-count_two(M), count_five(N)-count_five(N-M)-count_five(M)))
