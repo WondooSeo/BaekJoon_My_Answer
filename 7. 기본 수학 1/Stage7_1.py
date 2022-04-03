93202df2ab30708778440585ed84f494b19ddf31
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:46:14 +0900

    Question no. 1712

--- 7. 기본 수학 1/Stage7_1.py
+++ 7. 기본 수학 1/Stage7_1.py
@@ -1,8 +1,8 @@
 import sys
 
 if __name__ == '__main__':
-    A,B,C = map(int,sys.stdin.readline().rstrip().split())
-    if B-C>=0:
+    A, B, C = map(int, sys.stdin.readline().rstrip().split())
+    if B-C >= 0:
         print(-1)
     else:
         print(A//(C-B)+1)
