be48ac3ebbf08eac2a5acbaed43b45228a3c393b
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:43:40 +0900

    Question no. 2908

--- 6. 문자열/Stage6_7.py
+++ 6. 문자열/Stage6_7.py
@@ -1,14 +1,14 @@
 import sys
 
 if __name__ == '__main__':
-	a,b = map(str,sys.stdin.readline().rstrip().split())
-	a = list(map(int,a))
-	b = list(map(int,b))
+	a, b = map(str, sys.stdin.readline().rstrip().split())
+	a = list(map(int, a))
+	b = list(map(int, b))
 	a.reverse()
 	b.reverse()
 	a = 100*a[0] + 10*a[1] + a[2]
 	b = 100*b[0] + 10*b[1] + b[2]
-	if a>=b:
+	if a >= b:
 		print(a)
 	else:
 		print(b)
