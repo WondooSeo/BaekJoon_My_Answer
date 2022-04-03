ea2b88596f9bd68493205932c6356bbfb826be09
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:59:08 +0900

    Question no. 1085

--- 8. 기본 수학 2/Stage8_7.py
+++ 8. 기본 수학 2/Stage8_7.py
@@ -1,6 +1,6 @@
 import sys
 
 if __name__ == '__main__':
-    x,y,w,h = map(int,sys.stdin.readline().rstrip().split())
+    x, y, w, h = map(int, sys.stdin.readline().rstrip().split())
     wx = w-x; hy = h-y;
-    print(min(x,y,wx,hy))
+    print(min(x, y, wx, hy))
