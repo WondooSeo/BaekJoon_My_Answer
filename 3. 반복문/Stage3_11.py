d9ff50fdd5b4a092dc69b2bb8aa3cfaa4bd8f2da
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:29:24 +0900

    Question no. 10871

--- 3. 반복문/Stage3_11.py
+++ 3. 반복문/Stage3_11.py
@@ -1,9 +1,9 @@
 import sys
 
 if __name__ == '__main__':
-	n,x = map(int,sys.stdin.readline().rstrip().split())
-	a = list(map(int,sys.stdin.readline().rstrip().split()))
+	n, x = map(int, sys.stdin.readline().rstrip().split())
+	a = list(map(int, sys.stdin.readline().rstrip().split()))
 
 	for i in range(n):
-		if a[i]<x:
+		if a[i] < x:
 			print(a[i], end=' ')
