71c0a1b26a55db30024d429e6e1eb141c96a4ecf
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:16:40 +0900

    Question no. 2609

--- 16. 정수론 및 조합론/Stage16_3.py
+++ 16. 정수론 및 조합론/Stage16_3.py
@@ -1,7 +1,7 @@
 import sys
 
 if __name__ == '__main__':
-    a, b = map(int,sys.stdin.readline().rstrip().split())
+    a, b = map(int, sys.stdin.readline().rstrip().split())
     a_t = a; b_t = b;
     while a_t != 0:
         b_t = b_t%a_t
@@ -9,4 +9,4 @@ if __name__ == '__main__':
 
     gcd = b_t
     lcm = (a*b)//gcd
-    print(gcd,lcm)
+    print(gcd, lcm)
