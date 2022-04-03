ffcf0204643777bad7a1f227cc0247976dea5fb9
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 20:44:27 +0900

    Question no. 2751

---
+++ 11. 정렬/Stage11_2.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def merge_sort(num_arr):
+
+
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline().rstrip())
+    num_list = list()
+
+    for _ in range(N):
+        num_list.append(int(sys.stdin.readline().rstrip()))
+
