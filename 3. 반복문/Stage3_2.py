281450679bd907d40efb27a66d6ddabd9aacd88d
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:28:21 +0900

    Question no. 10950

--- 3. 반복문/Stage3_2.py
+++ 3. 반복문/Stage3_2.py
@@ -2,8 +2,8 @@ it = int(input())
 a = list()
 
 for i in range(it):
-	t1, t2 = map(int,input().split())
-	a.append((t1,t2))
+	t1, t2 = map(int, input().split())
+	a.append((t1, t2))
 
-for (t1,t2) in a:
-	print(t1+t2)
+for (t1, t2) in a:
+	print(t1 + t2)
