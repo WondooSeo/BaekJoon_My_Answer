34a2cc1824ab43e5b61692c605b38bdbcff89536
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:34:26 +0900

    Question no. 1546

--- 4. 1차원 배열/Stage4_5.py
+++ 4. 1차원 배열/Stage4_5.py
@@ -2,6 +2,6 @@ import sys
 
 if __name__ == '__main__':
 	a = int(input())
-	a_list = list(map(int,sys.stdin.readline().rstrip().split()))
+	a_list = list(map(int, sys.stdin.readline().rstrip().split()))
 	score = sum(a_list)
 	print(score/max(a_list)/len(a_list)*100)
