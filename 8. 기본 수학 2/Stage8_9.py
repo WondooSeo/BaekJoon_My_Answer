5cc17e82d4f80afc31a093ed0ab3d2997f9da8d1
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:59:24 +0900

    Question no. 4153

--- 8. 기본 수학 2/Stage8_9.py
+++ 8. 기본 수학 2/Stage8_9.py
@@ -2,10 +2,10 @@ import sys
 
 if __name__ == '__main__':
     while True:
-        A,B,C = map(int,sys.stdin.readline().rstrip().split())
+        A, B, C = map(int, sys.stdin.readline().rstrip().split())
         if A == 0 and B == 0 and C == 0:
             break
-        byun = sorted([A,B,C])
+        byun = sorted([A, B, C])
         if byun[2]**2 == byun[0]**2+byun[1]**2:
             print('right')
         else:
