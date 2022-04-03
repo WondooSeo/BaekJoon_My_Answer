ad6b4f0a7fdeb91947451f6484521aaf15a2d798
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:28:40 +0900

    Question no. 15552

--- 3. 반복문/Stage3_4.py
+++ 3. 반복문/Stage3_4.py
@@ -1,6 +1,6 @@
 import sys
-n = int(sys.stdin.readline().rstrip())
 
+n = int(sys.stdin.readline().rstrip())
 for i in range(n):
-	p1,p2 = map(int,sys.stdin.readline().rstrip().split())
+	p1, p2 = map(int, sys.stdin.readline().rstrip().split())
 	print(p1+p2)
