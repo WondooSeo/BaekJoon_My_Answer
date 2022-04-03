847b9160e84aea6fbe56735ba43ad3344fed4594
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:16:31 +0900

    Question no. 1037

--- 16. 정수론 및 조합론/Stage16_2.py
+++ 16. 정수론 및 조합론/Stage16_2.py
@@ -2,6 +2,5 @@ import sys
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline().rstrip())
-
-    real_yaksu = list(map(int,sys.stdin.readline().rstrip().split()))
+    real_yaksu = list(map(int, sys.stdin.readline().rstrip().split()))
     print(max(real_yaksu)*min(real_yaksu))
