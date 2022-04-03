49376520962627efa4da36397f3ea4082cd3e5a6
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:08:31 +0900

    Question no. 11651

--- 11. 정렬/Stage11_7.py
+++ 11. 정렬/Stage11_7.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
 	point_list = []
 
 	for _ in range(N):
-		point = list(map(int,sys.stdin.readline().rstrip().split()))
+		point = list(map(int, sys.stdin.readline().rstrip().split()))
 		point_list.append(point[::-1])
 
 	sorted_y = sorted(point_list)
 	
 	for i in range(N):
-		print(sorted_y[i][1],sorted_y[i][0])
+		print(sorted_y[i][1], sorted_y[i][0])
