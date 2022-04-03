7fc50bd4110470a2ed6ec873c8d76c586652c2c0
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:24:46 +0900

    Question no. 2884

--- 2. 조건문/Stage2_5.py
+++ 2. 조건문/Stage2_5.py
@@ -1,6 +1,6 @@
-h, m = map(int,input().split())
+h, m = map(int, input().split())
 
-if m-45 < 0:
+if m - 45 < 0:
 	m = m+15
 	if h-1 < 0:
 		h = 23
@@ -9,4 +9,4 @@ if m-45 < 0:
 else:
 	m = m-45
 
-print(h,m)
+print(h, m)
