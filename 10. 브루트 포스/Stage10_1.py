50c5478fc569c3a680e18f31bda3800741536339
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:03:33 +0900

    Question no. 2798

--- 10. 브루트 포스/Stage10_1.py
+++ 10. 브루트 포스/Stage10_1.py
@@ -1,14 +1,14 @@
 import sys
 
 if __name__ == '__main__':
-    N,M = map(int,sys.stdin.readline().rstrip().split())
-    card = list(map(int,sys.stdin.readline().rstrip().split()))
+    N, M = map(int, sys.stdin.readline().rstrip().split())
+    card = list(map(int, sys.stdin.readline().rstrip().split()))
     
     global_max = -1
 
     for i in range(N-2):
-        for j in range(i+1,N-1,1):
-            for k in range(j+1,N,1):
+        for j in range(i+1, N-1, 1):
+            for k in range(j+1, N, 1):
                 card_sum = card[i]+card[j]+card[k]
                 if card_sum <= M and card_sum > global_max:
                     global_max = card_sum
