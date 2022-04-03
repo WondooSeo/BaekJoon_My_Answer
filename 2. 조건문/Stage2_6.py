98a567ca24d7c01645294ee1972e5c82efed14c4
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:24:53 +0900

    Question no. 2525

--- 2. 조건문/Stage2_6.py
+++ 2. 조건문/Stage2_6.py
@@ -1,14 +1,14 @@
-h, m = map(int,input().split())
+h, m = map(int, input().split())
 c = int(input())
 
-m = m+c%60
-h = h+c//60
+m = m + c%60
+h = h + c//60
 
 rm = m%60
 ph = m//60
 h = ph+h
 
-if h>23:
-	h=h%24
+if h > 23:
+	h = h%24
 
-print(h,rm)
+print(h, rm)
