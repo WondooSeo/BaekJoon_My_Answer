f3c037c0b33d72ef3af8f727d803258a62743b96
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:08:22 +0900

    Question no. 11650

--- 11. 정렬/Stage11_6.py
+++ 11. 정렬/Stage11_6.py
@@ -2,13 +2,13 @@ import sys
 
 if __name__ == '__main__':
     N = int(input())
-    point_list = []
+    point_list = list()
 
     for _ in range(N):
-        point = list(map(int,sys.stdin.readline().rstrip().split()))
+        point = list(map(int, sys.stdin.readline().rstrip().split()))
         point_list.append(point)
 
     point_list = sorted(point_list)
 
     for i in range(N):
-        print(point_list[i][0],point_list[i][1])
+        print(point_list[i][0], point_list[i][1])
