8e9efe4e0f64d0e1e7dbdadd9846bbfcef00e91d
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:24:58 +0900

    Question no. 2480

--- 2. 조건문/Stage2_7.py
+++ 2. 조건문/Stage2_7.py
@@ -1,14 +1,14 @@
-a,b,c = map(int,input().split())
+a, b, c = map(int, input().split())
 
-if a==b and b==c and c==a:
+if a == b and b == c and c == a:
 	p = 10000+a*1000
-elif a==b and b!=c and a!=c:
+elif a == b and b != c and a != c:
 	p = 1000+a*100
-elif a!=b and b==c and a!=c:
+elif a != b and b == c and a != c:
 	p = 1000+b*100
-elif a!=b and b!=c and a==c:
+elif a != b and b != c and a == c:
 	p = 1000+c*100
 else:
-	p = max(a,b,c)*100
+	p = max(a, b, c)*100
 
 print(p)
