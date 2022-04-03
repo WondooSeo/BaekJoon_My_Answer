e2fb08878327e25dd5e43f4a8b43df8c5bf873a7
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:27:00 +0900

    Question no. 2164

--- 18. 큐, 덱/Stage18_2.py
+++ 18. 큐, 덱/Stage18_2.py
@@ -3,8 +3,7 @@ from collections import deque
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline().rstrip())
-    
-    card_deque = deque(list(range(1,N+1)))
+    card_deque = deque(list(range(1, N+1)))
 
     while len(card_deque) > 1:
         card_deque.popleft()
