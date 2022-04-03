a5dcace00196c2c73bef991d1f0b1b195c4f7fe3
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:10:26 +0900

    Question no. 1932

--- 14. 동적 계획법 1/Stage14_6.py
+++ 14. 동적 계획법 1/Stage14_6.py
@@ -1,13 +1,14 @@
 import sys
 
+
 def int_tri(N):
     input_tri = list()
     for i in range(N):
-        input_tri.append(list(map(int,sys.stdin.readline().rstrip().split())))
+        input_tri.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
     for i in reversed(range(N-1)):
         for j in range(len(input_tri[i])):
-            input_tri[i][j] += max(input_tri[i+1][j],input_tri[i+1][j+1])
+            input_tri[i][j] += max(input_tri[i+1][j], input_tri[i+1][j+1])
 
     print(input_tri[0][0])
 
