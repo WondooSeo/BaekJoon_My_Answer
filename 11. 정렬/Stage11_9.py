bb726861ed2c0b12456d4eaab476f3a2067a21c2
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:08:38 +0900

    Question no. 10814

--- 11. 정렬/Stage11_9.py
+++ 11. 정렬/Stage11_9.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
 	for _ in range(N):
 		signin_list.append(list(sys.stdin.readline().rstrip().split()))
 
-	signin_list = sorted(signin_list,key=lambda x: (int(x[0])))
+	signin_list = sorted(signin_list, key=lambda x: (int(x[0])))
 	for signin in signin_list:
-		print(signin[0],signin[1])
+		print(signin[0], signin[1])
