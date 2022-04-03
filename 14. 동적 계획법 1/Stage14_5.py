8ad94ded733303759dc5be15303037bfbea65b42
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:10:23 +0900

    Question no. 1149

--- 14. 동적 계획법 1/Stage14_5.py
+++ 14. 동적 계획법 1/Stage14_5.py
@@ -1,16 +1,18 @@
 import sys
 
+
 def RGB_cost(n):
     cost_list = list()
     for _ in range(n):
-        cost_list.append(list(map(int,sys.stdin.readline().rstrip().split())))
+        cost_list.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
-    for i in range(1,n):
-        cost_list[i][0] = min(cost_list[i-1][1],cost_list[i-1][2]) + cost_list[i][0]
-        cost_list[i][1] = min(cost_list[i-1][0],cost_list[i-1][2]) + cost_list[i][1]
-        cost_list[i][2] = min(cost_list[i-1][0],cost_list[i-1][1]) + cost_list[i][2]
+    for i in range(1, n):
+        cost_list[i][0] = min(cost_list[i-1][1], cost_list[i-1][2]) + cost_list[i][0]
+        cost_list[i][1] = min(cost_list[i-1][0], cost_list[i-1][2]) + cost_list[i][1]
+        cost_list[i][2] = min(cost_list[i-1][0], cost_list[i-1][1]) + cost_list[i][2]
 
     print(min(cost_list[n-1]))
+
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline().rstrip())
