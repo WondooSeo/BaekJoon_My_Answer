d6d43c5a1566f3075a3c8f9e512330fe90f92d65
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:08:52 +0900

    Question no. 18870

--- 11. 정렬/Stage11_10.py
+++ 11. 정렬/Stage11_10.py
@@ -2,8 +2,8 @@ import sys
 
 if __name__ == '__main__':
     a = int(input())
-    b_list = list(map(int,sys.stdin.readline().rstrip().split()))
+    b_list = list(map(int, sys.stdin.readline().rstrip().split()))
     b_list_sort = list(sorted(set(b_list)))
-    dic_b = {b_list_sort[i]:i for i in range(len(b_list_sort))}
+    dic_b = {b_list_sort[i]: i for i in range(len(b_list_sort))}
     for i in b_list:
-        print(dic_b[i],end=' ')
+        print(dic_b[i], end=' ')
