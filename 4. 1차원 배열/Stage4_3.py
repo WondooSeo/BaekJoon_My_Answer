52a20a4e9b9310f1432f7f17468e311394251adb
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 16:34:16 +0900

    Question no. 2577

--- 4. 1차원 배열/Stage4_3.py
+++ 4. 1차원 배열/Stage4_3.py
@@ -4,9 +4,9 @@ if __name__ == '__main__':
 	a = int(input())
 	b = int(input())
 	c = int(input())
-	r = list(map(int,str(a*b*c)))
+	r = list(map(int, str(a*b*c)))
 	ans = [0]*10
 	for i in range(len(r)):
-		ans[r[i]]+=1
+		ans[r[i]] += 1
 	for i in range(len(ans)):
 		print(ans[i])
