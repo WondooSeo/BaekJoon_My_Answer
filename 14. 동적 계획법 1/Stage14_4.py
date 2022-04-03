7924cb5e6266011c32b212a556e672ea0f974584
WDSeo <wdseo@yonsei.ac.kr>
2022-04-03 17:10:19 +0900

    Question no. 9461

--- 14. 동적 계획법 1/Stage14_4.py
+++ 14. 동적 계획법 1/Stage14_4.py
@@ -1,12 +1,14 @@
 import sys
 
+
 def padoban(num):
-    padoban_list = [1,1,1]
+    padoban_list = [1, 1, 1]
     if num > len(padoban_list):
-        for i in range(len(padoban_list),num):
+        for i in range(len(padoban_list), num):
             padoban_list.append(padoban_list[i-2]+padoban_list[i-3])
 
     print(padoban_list[num-1])
+
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline().rstrip())
